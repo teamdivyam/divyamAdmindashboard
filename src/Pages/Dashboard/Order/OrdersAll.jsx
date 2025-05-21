5551376b35a159cc718eabd32185f8d75a151fd4
Mahtab <mahtabbhai420@gmail.com>
2025-05-21 11:58:00 +0530

    update-order-components

--- src/Pages/Dashboard/Order/OrdersAll.jsx
+++ src/Pages/Dashboard/Order/OrdersAll.jsx
@@ -211,160 +211,162 @@ export default function LinksTable() {
 
   return (
     <>
-      <div className="w-full ">
-        <Toaster richColors />
-        <form
-          className="flex justify-center  w-full px-3 py-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Search with order id"
-            className="border px-4 shadow-sm  w-full rounded-lg text-lg rounded-r-none dark:bg-slate-600"
-            {...register("searchKey")}
-          />
-          <button
-            type="submit"
-            className="bg-orange-400 rounded-md p-2 text-white rounded-l-none"
+      {Orders && (
+        <div className="w-full ">
+          <Toaster richColors />
+          <form
+            className="flex justify-center  w-full px-3 py-4"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            <Search className="size-8" />
-          </button>
-        </form>
-        <Table>
-          <TableCaption>.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[300px]">Order Id</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Payment Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
-              <TableHead className="text-right">Transaction Date</TableHead>
-              {/* <TableHead className="text-right">Actions</TableHead> */}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {Orders && Orders.length > 0 ? (
-              Orders.map((order, idx) => {
-                const date = new Date(order.createdAt);
-                const day = String(date.getDate()).padStart(2, "0");
-                const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
-                const year = date.getFullYear();
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search with order id"
+              className="border px-4 shadow-sm  w-full rounded-lg text-lg rounded-r-none dark:bg-slate-600"
+              {...register("searchKey")}
+            />
+            <button
+              type="submit"
+              className="bg-orange-400 rounded-md p-2 text-white rounded-l-none"
+            >
+              <Search className="size-8" />
+            </button>
+          </form>
+          <Table>
+            <TableCaption>.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[300px]">Order Id</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Payment Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+                <TableHead className="text-right">Transaction Date</TableHead>
+                {/* <TableHead className="text-right">Actions</TableHead> */}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Orders && Orders.length > 0 ? (
+                Orders.map((order, idx) => {
+                  const date = new Date(order.createdAt);
+                  const day = String(date.getDate()).padStart(2, "0");
+                  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+                  const year = date.getFullYear();
 
-                // Format the date as dd/mm/yyyy
-                const formattedDate = `${day}/${month}/${year}`;
+                  // Format the date as dd/mm/yyyy
+                  const formattedDate = `${day}/${month}/${year}`;
 
-                return (
-                  <TableRow key={idx}>
-                    <TableCell className="font-medium">
-                      <NavLink to={`${order._id}`}>
-                        {order.orderId.split("_").at(1)}
-                      </NavLink>
-                    </TableCell>
-                    <TableCell>
-                      {changeOrderStatusColor(order.orderStatus)}
-                    </TableCell>
-                    <TableCell>{order.payment.method}</TableCell>
-                    <TableCell className="text-right">
-                      {order.totalAmount}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      {formattedDate}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <DropdownMenu>
-                        <DropdownMenuTrigger>
-                          <EllipsisVertical />
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                          <DropdownMenuLabel>Action</DropdownMenuLabel>
-                          <DropdownMenuSeparator />
-                          <DropdownMenuItem className="cursor-pointer">
-                            <NavLink to={`${order._id}`}>View More</NavLink>
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-            ) : (
-              <h1>0 Orders available..</h1>
-            )}
-          </TableBody>
-        </Table>
+                  return (
+                    <TableRow key={idx}>
+                      <TableCell className="font-medium">
+                        <NavLink to={`${order._id}`}>
+                          {order.orderId.split("_").at(1)}
+                        </NavLink>
+                      </TableCell>
+                      <TableCell>
+                        {changeOrderStatusColor(order.orderStatus)}
+                      </TableCell>
+                      <TableCell>{order.payment.method}</TableCell>
+                      <TableCell className="text-right">
+                        {order.totalAmount}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {formattedDate}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            <EllipsisVertical />
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent>
+                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem className="cursor-pointer">
+                              <NavLink to={`${order._id}`}>View More</NavLink>
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
+              ) : (
+                <h1>0 Orders available..</h1>
+              )}
+            </TableBody>
+          </Table>
 
-        {/* ORDER_PAGINATION */}
+          {/* ORDER_PAGINATION */}
 
-        {Orders && Orders.length > 10 ? (
-          <Pagination>
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  className="cursor-pointer"
-                  onClick={() => {
-                    {
-                      state?.page == 1
-                        ? null
-                        : dispatch({ type: "FIRST_PAGE" });
-                    }
-                  }}
-                ></PaginationPrevious>
-              </PaginationItem>
+          {Orders && Orders.length > 10 ? (
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious
+                    className="cursor-pointer"
+                    onClick={() => {
+                      {
+                        state?.page == 1
+                          ? null
+                          : dispatch({ type: "FIRST_PAGE" });
+                      }
+                    }}
+                  ></PaginationPrevious>
+                </PaginationItem>
 
-              <PaginationItem>
-                <PaginationLink
-                  className="cursor-pointer"
-                  isActive
-                  onClick={() => {
-                    dispatch({ type: "SET_PAGE_NUM", payload: 1 });
-                  }}
-                >
-                  1
-                </PaginationLink>
-              </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink
+                    className="cursor-pointer"
+                    isActive
+                    onClick={() => {
+                      dispatch({ type: "SET_PAGE_NUM", payload: 1 });
+                    }}
+                  >
+                    1
+                  </PaginationLink>
+                </PaginationItem>
 
-              <PaginationItem>
-                <PaginationLink
-                  className="cursor-pointer"
-                  isActive
-                  onClick={() => {
-                    dispatch({ type: "SET_PAGE_NUM", payload: 2 });
-                  }}
-                >
-                  2
-                </PaginationLink>
-              </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink
+                    className="cursor-pointer"
+                    isActive
+                    onClick={() => {
+                      dispatch({ type: "SET_PAGE_NUM", payload: 2 });
+                    }}
+                  >
+                    2
+                  </PaginationLink>
+                </PaginationItem>
 
-              <PaginationItem>
-                <PaginationLink
-                  className="cursor-pointer"
-                  isActive
-                  onClick={() => {
-                    dispatch({ type: "SET_PAGE_NUM", payload: 3 });
-                  }}
-                >
-                  3
-                </PaginationLink>
-              </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink
+                    className="cursor-pointer"
+                    isActive
+                    onClick={() => {
+                      dispatch({ type: "SET_PAGE_NUM", payload: 3 });
+                    }}
+                  >
+                    3
+                  </PaginationLink>
+                </PaginationItem>
 
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
 
-              <PaginationItem>
-                <PaginationNext
-                  className="cursor-pointer"
-                  onClick={() => {
-                    dispatch({ type: "INC_PAGE" });
-                  }}
-                ></PaginationNext>
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        ) : null}
-      </div>
+                <PaginationItem>
+                  <PaginationNext
+                    className="cursor-pointer"
+                    onClick={() => {
+                      dispatch({ type: "INC_PAGE" });
+                    }}
+                  ></PaginationNext>
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          ) : null}
+        </div>
+      )}
     </>
   );
 }
