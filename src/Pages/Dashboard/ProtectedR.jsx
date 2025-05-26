c098cc03b8511aa6ea934161b283946459fff51e
Mahtab <mahtabbhai420@gmail.com>
2025-05-26 11:39:33 +0530

    fix:login-page-auto-redirect-after-token-expire

--- src/Pages/Dashboard/ProtectedR.jsx
+++ src/Pages/Dashboard/ProtectedR.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useMemo } from "react";
+import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import { isAuth } from "../../store/Auth/Authentication";
 import isTokenExpired from "../../utils/isTokenExpired.js";
 
 const ProtectedRoute = ({ children }) => {
-  const navigate = useNavigate();
-  const token = localStorage.getItem("AppID") || null;
-  const isTokenEXP = isTokenExpired(token);
-
-  if (token || isTokenEXP == false) {
-    return children;
+  const token = localStorage.getItem("AppID");
+  if (!token || isTokenExpired(token)) {
+    return <Navigate to="/login" />;
   }
 
-  if (isTokenEXP == false) {
-    return children;
-  }
-
-  useEffect(() => {
-    if (isTokenEXP) {
-      navigate("/login");
-      return;
-    }
-
-    if (isTokenEXP && token) {
-      return children;
-    }
-  }, [isTokenEXP, token]);
-
-  return <Navigate to="/login" />;
+  return children;
 };
 
 export default ProtectedRoute;
